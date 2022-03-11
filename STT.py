0a41845afaf1557be89489d095326cf0cc5a3dc9
MontBru <80487370+MontBru@users.noreply.github.com>
2022-03-11 11:56:00 +0200

    Update STT.py

--- STT.py
+++ STT.py
@@ -3,22 +3,14 @@ import pyttsx3
 import os
 import sys
 import keyboard
-
-recogniser=speech_recognition.Recognizer()
-button=False
-text=""
-while True:
-    if keyboard.read_key()=="a":
-        button = True
-        break
-
-while button==True:
-
-    try:
-        if keyboard.read_key()=="s":
-            button = False
-            break
-        if button == True:
+from datetime import datetime
+def SpeechToText():
+    recogniser=speech_recognition.Recognizer()
+    text=""
+    while True:
+        try:
+            if recording.in_progress==False:
+                break
             with  speech_recognition.Microphone() as mic:
                 recogniser.adjust_for_ambient_noise(mic)
                 audio=recogniser.listen(mic)
@@ -26,22 +18,20 @@ while button==True:
                 text=recogniser.recognize_google(audio)
                 text=text.lower()
 
-                
+                    
 
-    except Exception:
-        continue
+        except Exception:
+            continue
 
 #print(f"Recognised {text}")
-index_file=open(os.path.join(sys.path[0],"STT_Index_File.txt"),"r")
-try:
-    index=int(index_file.read())
-    index_file=open(os.path.join(sys.path[0],"STT_Index_File.txt"),"w")
-    index_file.write(str(index+1))
-    index_file.close()
+#index_file=open(os.path.join(sys.path[0],"STT_Index_File.txt"),"r")
+#try:
+#    index=int(index_file.read())
+#    index_file=open(os.path.join(sys.path[0],"STT_Index_File.txt"),"w")
+#    index_file.write(str(index+1))
+#    index_file.close()
 
-    name="STT_Original_"+str(index)+".txt"
+    name=sys.path[0]+"\\Recordings\\TextFile_" + str(datetime.now().strftime("%H_%M_%S"))+".txt"
     f=open(name,"w")
     f.write(text)
     f.close()
-except ValueError:
-    pass
