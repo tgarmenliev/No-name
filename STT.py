b06a8881e87b54be497268042864cea7b16b6566
MontBru <80487370+MontBru@users.noreply.github.com>
2022-03-11 08:30:26 +0200

    Update STT.py

--- STT.py
+++ STT.py
@@ -5,19 +5,18 @@ recogniser=speech_recognition.Recognizer()
 button=False
 
 
-while button==True:
+while True:
 
     try:
-        with  speech_recognition.Microphone() as mic:
-            recogniser.adjust_for_ambient_noise(mic,duration=0.2)
-            audio=recogniser.listen(mic)
+        if button == True:
+            with  speech_recognition.Microphone() as mic:
+                recogniser.adjust_for_ambient_noise(mic,duration=0.2)
+                audio=recogniser.listen(mic)
 
-            text=recogniser.recognize_google(audio)
-            text=text.lower()
+                text=recogniser.recognize_google(audio)
+                text=text.lower()
 
-            print(f"Recognised {text}")
+                print(f"Recognised {text}")
 
     except Exception:
         continue
-
-
