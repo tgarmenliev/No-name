ba2fe966de148fdcfdaf8082fb6fa491f0a5ba60
Искрен <32600231+Quant14@users.noreply.github.com>
2022-03-11 09:26:40 +0200

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-from gtts import gTTS
-import os
-file_path = "C:/Users/tisho/Desktop/Python_files/test.txt"
-f = open(file_path, 'r')
-str = f.read()
-tts = gTTS(text=str, lang='en')
-f.close()
-tts.save("good.mp3")
-os.system("start good.mp3")
