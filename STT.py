ee77f23ea98c5dfd08b8b8c795c7c377dd6f8785
MontBru <80487370+MontBru@users.noreply.github.com>
2022-03-11 10:14:42 +0200

    Update STT.py

--- STT.py
+++ STT.py
@@ -2,14 +2,22 @@ import speech_recognition
 import pyttsx3
 import os
 import sys
+import keyboard
 
 recogniser=speech_recognition.Recognizer()
 button=False
-
+text=""
+while True:
+    if keyboard.read_key()=="a":
+        button = True
+        break
 
 while button==True:
 
     try:
+        if keyboard.read_key()=="s":
+            button = False
+            break
         if button == True:
             with  speech_recognition.Microphone() as mic:
                 recogniser.adjust_for_ambient_noise(mic)
@@ -18,11 +26,12 @@ while button==True:
                 text=recogniser.recognize_google(audio)
                 text=text.lower()
 
-                #print(f"Recognised {text}")
+                
 
     except Exception:
         continue
 
+#print(f"Recognised {text}")
 index_file=open(os.path.join(sys.path[0],"STT_Index_File.txt"),"r")
 try:
     index=int(index_file.read())
@@ -30,7 +39,7 @@ try:
     index_file.write(str(index+1))
     index_file.close()
 
-    name="STT_Original_"+str(index)
+    name="STT_Original_"+str(index)+".txt"
     f=open(name,"w")
     f.write(text)
     f.close()
